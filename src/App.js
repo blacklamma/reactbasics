5abdbf98db67da65d4cf9b4354745ba57486c185
blacklamma <rudradeep1998@gmail.com>
2020-10-24 20:03:05 +0530

    added class component for welcome

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
-import Greet from './components/Greet'
+// import Greet from './components/Greet'
+import Welcome from './components/Welcome'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <Greet />
+        <Welcome />
     </div>
   );
 }
